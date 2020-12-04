1e5a228c01eacf1966ee7a65e940942abcd5fb2b
Takuya Yamamoto <59943729+oozzZZZZ@users.noreply.github.com>
2020-12-04 17:22:51 +0900

    Update step0.py
    データオーギュメンテーション機能の追加

--- step0.py
+++ step0.py
@@ -1,55 +1,159 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 28 18:51:05 2020
+Created on Sat Nov 28 18:46:45 2020
 
 @author: yamamoto
 """
-
-"""
-Step 0
-学習前のデータ処理フェーズ
-"""
-
-import os
-import random
 import torch
+import librosa
+import numpy as np
+from tqdm import tqdm
+import random
+import os
+import glob
+import parameter
+p=parameter.Parameter()
 
-import step_utils as ut
-from parameter import Parameter
+def take_path(path):
+    data_list = []
+    for a,b,c in os.walk(path):
+        l = glob.glob(a+"/*.wav" ,recursive=True)
+        data_list = data_list + l
+    return data_list
 
-def main():
-    print("#####################################################################")
-    print("Step0 Make Datasets Phase")
-    print("#####################################################################")
+# data augumentation tools
+
+def time_shift(data,sample_rate,shift):
+    data_roll = np.roll(data, shift)
+    return data_roll
+
+def stretch(data, rate=1):
+    input_length = len(data)
+    data = librosa.effects.time_stretch(data, rate)
+    if len(data)>input_length:
+        data = data[:input_length]
+    else:
+        data = np.pad(data, (0, max(0, input_length - len(data))), "constant")
+
+    return data
+
+def pitch_shift(data,sample_rate,shift):
+    """
+    shiftについて
+    +12:1オクターブ上
+    -12:1オクターブ下
+    +7:完全５度
+    +5:完全４度
+    """
+    ret = librosa.effects.pitch_shift(data, sample_rate, shift, bins_per_octave=12, res_type='kaiser_best')
+    return ret
+
+def length_fitting(data,audio_len):
+    if len(data) > audio_len:
+        data = data[:audio_len]
+    else: 
+        while len(data) < audio_len:
+            data = torch.cat((data,data),0)[:audio_len]
+    return data
     
-    p=Parameter()
-    audio_len = p.audio_len
-    sample_rate = p.sample_rate
-    noise_snr = p.noise_rate 
-    clean_speech_dir = p.target_path
-    noise_dir = p.noise_path
-    datasets_save_dir = p.datasets_path
+def wav2tensorstft(filename,audio_len,sample_rate):
+    data, _  = librosa.load(filename, sr=sample_rate)
+    data=torch.from_numpy(data.astype(np.float32)).clone()
+    data = length_fitting(data,audio_len)
+    stftdata = torch.stft(data,2**8,return_complex=True)
+    realpart = stftdata.real
+    imagpart = stftdata.imag
+    c = torch.cat((realpart,imagpart),0)
+    return c
+
+def dataloading_skip(data,audio_len): # 音声データが破損してそうな場合スキップしたい
+    data=torch.from_numpy(data.astype(np.float32)).clone()
+    data = length_fitting(data,audio_len)
+    stftdata = torch.stft(data,2**8,return_complex=True)
+    realpart = stftdata.real
+    imagpart = stftdata.imag
+    c = torch.cat((realpart,imagpart),0)
+    return c    
+
+def tensorstft2audio(stftdata):
+    h = int(stftdata.shape[0]/2)
+    z = torch.complex(stftdata[:h,:], stftdata[h:,:])
+    tensoraudio = torch.istft(z,2**8)
+    numpyaudio = tensoraudio.to('cpu').detach().numpy().copy()
+    return numpyaudio
     
+def addnoise(speech_stft,noise_stft,noise_snr):
+    nsr_noise_stft = noise_stft * noise_snr
+    addnoise = torch.add(speech_stft,nsr_noise_stft)
+    return addnoise
+
+def make_stack(c_files,n_files,audio_len,sample_rate,noise_snr,augmentation_mode):
+    speech_list = []
+    speech_noise_list = []
     
-    if not os.path.exists(datasets_save_dir):
-        os.mkdir(datasets_save_dir)
+    num_c_files = len(c_files)
+    num_n_files = len(n_files)
     
-    c_files = ut.take_path(clean_speech_dir)
-    n_files = ut.take_path(noise_dir)
+    print("\nclean speech data is {} files \nNoise data is {} files"
+          .format(num_c_files,num_n_files))        
+    
+    for c in tqdm(c_files):
+        n = n_files[random.randint(0, num_n_files-1)]
         
-    random.shuffle(c_files)
-    random.shuffle(n_files)
+        # 音声データが破損してそうな場合スキップしたい
+        c_data, _  = librosa.load(c, sr=sample_rate)
+        n_data, _  = librosa.load(n, sr=sample_rate)
+        
+        skip_count = 0
+        
+        #破損データのスキップ
+        if len(c_data) > audio_len and len(n_data) > 10:
+                #データ水増しモードオン
+                if augmentation_mode == True:
+                    step = len(c_data) // audio_len
+                    for i in range(step):
+                        for w in range(p.augmentation_num):
+                            c_p = c_data[i*audio_len : (i+1)*audio_len]
+                            c_p = stretch(c_p, rate=random.uniform(0.5, 1.5)) #ランダムデータ伸縮
+                            c_p = pitch_shift(c_p,16000,random.uniform(-12, 12)) #ランダムピッチシフト
+                            c_p = c_p * random.uniform(0.8, 0.8) #ランダム音量変更
+                            
+                            c_stft = dataloading_skip(c_p,audio_len)
+                            n_stft = dataloading_skip(n_data,audio_len)
+                            c_n_stft = addnoise(c_stft,n_stft,noise_snr)
+                            
+                            speech_list.append(c_stft)
+                            speech_noise_list.append(c_n_stft)
+                    
+                #データ水増しモードオフ    
+                else:
+                    step = len(c_data) // audio_len
+                    for w in range(p.augmentation_num):
+                        c_p = c_data[i*audio_len : (i+1)*audio_len]
+                    
+                        c_stft = dataloading_skip(c_p,audio_len)
+                        n_stft = dataloading_skip(n_data,audio_len)
+                        c_n_stft = addnoise(c_stft,n_stft,noise_snr)
+                        speech_list.append(c_stft)
+                        speech_noise_list.append(c_n_stft)
+
+        else:
+            skip_count += 1
     
-    tensor_speech,tensor_addnoise = ut.make_stack(c_files,n_files,audio_len,sample_rate,noise_snr)
-    print(tensor_speech.shape,tensor_addnoise.shape)
+    #データの出力処理
+        
+    num_data = len(speech_list)
+    a = round(num_data, -2)
+    if a > num_data:  
+        usedata_num = round(num_data-100, -2)
+
+    tensor_speech = torch.stack(speech_list[:usedata_num])
+    tensor_addnoise = torch.stack(speech_noise_list[:usedata_num])
     
-    torch.save(tensor_speech,datasets_save_dir+"/tensor_speech")
-    torch.save(tensor_addnoise,datasets_save_dir+"/tensor_addnoise")
+    print("Maybe Corrupt Data :",skip_count)
+    print("Available data :", num_data)
+    print("Use data :", num_data)
     
-    print("#####################################################################")
-    print("Step0 All completed!!!")
-    print("#####################################################################")
+    return tensor_speech,tensor_addnoise
 
 
-if __name__ == '__main__':
-    main()
