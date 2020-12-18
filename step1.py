c5ff4ee15d83c8e62eba35b7c80778c69060f701
Takuya Yamamoto <59943729+oozzZZZZ@users.noreply.github.com>
2020-12-18 18:33:33 +0900

    新バージョン

--- step1.py
+++ step1.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 28 20:25:43 2020
+Created on Fri Dec 18 17:31:07 2020
 
-@author: yamamoto
+@author: t.yamamoto
 """
 
 """
@@ -15,19 +16,18 @@ import torch.utils.data as utils
 import torch.optim as optim
 
 import os
+import numpy as np
 import datetime
+import glob
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 
 import model as mm
 import parameter
 
 def main():
-    print("#####################################################################")
-    print("Step1 Training Phase")
-    print("#####################################################################")
-    
+
     p=parameter.Parameter()
+    
     datasets_save_dir = p.datasets_path
     model_save_dir = p.model_path
     split = p.datasets_split
@@ -36,25 +36,46 @@ def main():
     num_layer = p.num_layer
     epochs = p.epochs
     
-    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
+    
     print("CUDA is available:", torch.cuda.is_available())
-    print("\nLoading Datasets.....")
     
     if not os.path.exists(model_save_dir):
         os.mkdir(model_save_dir)
         
-    #保存モデル名の定義
     now = datetime.datetime.now()
     model_path = model_save_dir+"/model_layer"+str(num_layer)+"_"+now.strftime('%Y%m%d_%H%M%S')+".pt"
+    
+    _datasets_path = glob.glob(datasets_save_dir+"/*.npz")
+    
+    speech_list = []
+    addnoise_list = []
+    
+    for file in tqdm(_datasets_path):
+        d = np.load(file)    
+        speech=torch.from_numpy(d["speech"].astype(np.float32)).clone()
+        addnoise=torch.from_numpy(d["addnoise"].astype(np.float32)).clone()
         
-    tensor_speech = torch.load(datasets_save_dir+"/tensor_speech")
-    tensor_addnoise = torch.load(datasets_save_dir+"/tensor_addnoise")
+        speech_list.append(speech)
+        addnoise_list.append(addnoise)
+        
+    num_data = len(speech_list)
+    a = round(num_data, -2)
+    if a > num_data:  
+        num_usedata = round(num_data-100, -2)
+        
+    tensor_speech = torch.stack(speech_list[:num_usedata])
+    tensor_addnoise = torch.stack(addnoise_list[:num_usedata])
+    
+    print("Available data :", num_data)
+    print("Use data :", num_usedata)
     
     mydataset = utils.TensorDataset(tensor_speech,tensor_addnoise)
     data_num = tensor_speech.shape[0]
-    data_split = [data_num * split[0],data_num * split[1],data_num * split[2]]
+    data_split = [int(data_num * split[0]),
+                  int(data_num * split[1]),
+                  int(data_num * split[2])]
     test_dataset,val_dataset,train_dataset = utils.random_split(mydataset,data_split)
     
     train_loader = utils.DataLoader(train_dataset,batch_size=batch_size,num_workers=os.cpu_count(),pin_memory=True,shuffle=True)
@@ -71,22 +92,21 @@ def main():
     # criterion = nn.MSELoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_late)
     
-    
     print("#####################################################################")
     print(" Start Training..")
     print("#####################################################################")
-
-
+    
+    
     train_loss_list = []
     test_loss_list = []
     
     for epoch in tqdm(range(1, epochs+1),desc='[Training..]'):
-        
-        
+    
+    
         # Training
     
         model.train()  # 訓練モードに
-        
+    
         train_loss = 0
         for batch_idx, (speech, addnoise) in enumerate(train_loader):
             # データ取り出し
@@ -102,7 +122,7 @@ def main():
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
-        
+    
         train_loss /= len(train_loader.dataset)
         train_loss_list.append(train_loss)
     
@@ -124,30 +144,20 @@ def main():
     
             tqdm.write('\nTrain set: Average loss: {:.6f}\nTest set:  Average loss: {:.6f}'
                        .format(train_loss,test_loss))
-            
+    
         if epoch == 1:
             best_loss = test_loss
             torch.save(model.state_dict(), model_path)
-            
+    
         else:
             if best_loss > test_loss:
                 torch.save(model.state_dict(), model_path)
                 best_loss = test_loss
-            
-         
+    
+    
         if epoch % 10 == 0: #10回に１回定期保存
             epoch_model_path = model_save_dir+"/model_layer"+str(num_layer)+"_"+now.strftime('%Y%m%d_%H%M%S')+"_Epoch"+str(epoch)+".pt"
             torch.save(model.state_dict(), epoch_model_path)
-        
-
-    
-    fig,ax = plt.subplot()
-    ax.plot(train_loss_list,linewidth=2, color="red" ,label="Train Loss")
-    ax.plot(test_loss_list,linewidth=2, color="blue" ,label="Test Loss")
-    ax.legend(loc='upper right')
-    fig.tight_layout()
-    fig.savefig(model_save_dir+"/model_layer"+str(num_layer)+"_"+now.strftime('%Y%m%d_%H%M%S')+".png")
-
-
+            
 if __name__ == '__main__':
     main()
